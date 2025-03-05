5806e29fb0ca49c9c9f1963be041d7dcc8497428
tony <ybhavaniprasad79@gmail.com>
2025-03-05 14:21:00 +0530

    done

---
+++ assignment/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <div className="bg-gray-800 p-10 rounded-2xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold mb-2">Y. Bhavani Prasad</h1>
+        <p className="text-lg">Student at Kalvium</p>
+        <p className="text-md mt-4">📞 Phone: 8978655991</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
